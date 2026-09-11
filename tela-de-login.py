06d02e19fcdbfb0fd4927f214d4203a0514b67eb
Gabriela Andrade <gabrielaandrade1890@gmail.com>
2026-09-11 16:13:05 -0300

    update: versao-final

--- tela-de-login.py
+++ tela-de-login.py
@@ -3,8 +3,8 @@ from PySimpleGUI import PySimpleGUI as sg
 # Layout
 sg.theme('Reddit')
 layout = [
-    [sg.Text('Usuário'), sg.Input(key='usuario')],
-    [sg.Text('Senha'), sg.Input(key='senha', password_char='*')],
+    [sg.Text('Usuário'), sg.Input(key='usuario', size=(20, 1))],
+    [sg.Text('Senha'), sg.Input(key='senha', password_char='*', size=(20, 1))],
     [sg.Checkbox('Salvar o login?')],
     [sg.Button('Entrar')]
 ]
