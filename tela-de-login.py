befcdfcf2739131132d6c5ccc095120a7bfed1ae
Gabriela Andrade <gabrielaandrade1890@gmail.com>
2026-09-11 15:52:00 -0300

    feature: interface-completa
    tela de login completa!

---
+++ tela-de-login.py
@@ -0,0 +1,20 @@
+from PySimpleGUI import PySimpleGUI as sg
+
+# Layout
+sg.theme('Reddit')
+layout = [
+    [sg.Text('Usuário'), sg.Input(key='usuario')],
+    [sg.Text('Senha'), sg.Input(key='senha', password_char='*')],
+    [sg.Checkbox('Salvar o login?')],
+    [sg.Button('Entrar')]
+]
+# Janela
+janela = sg.Window('Tela de Login', layout)
+# Ler os eventos
+while True:
+    eventos, valores = janela.read()
+    if eventos == sg.WINDOW_CLOSED:
+        break
+    if eventos == 'Entrar':
+        if valores['usuario'] == 'gabi' and valores['senha'] == '2510':
+            print('Bem-vindo ao programa!')
